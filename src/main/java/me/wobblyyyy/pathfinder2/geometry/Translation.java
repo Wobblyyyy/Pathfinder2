eab8bdab00662e4d97148e5520ef35034ecb03b5
colin <wobblyyyy@gmail.com>
2021-06-10 16:32:16 -0400

    documentation!

--- src/main/java/me/wobblyyyy/pathfinder2/geometry/Translation.java
+++ src/main/java/me/wobblyyyy/pathfinder2/geometry/Translation.java
@@ -1,3 +1,13 @@
+/*
+ * Copyright (c) 2021.
+ *
+ * This file is part of the "Pathfinder2" project, available here:
+ * <a href="https://github.com/Wobblyyyy/Pathfinder2">GitHub</a>
+ *
+ * This project is licensed under the GNU GPL V3 license.
+ * <a href="https://www.gnu.org/licenses/gpl-3.0.en.html">GNU GPL V3</a>
+ */
+
 package me.wobblyyyy.pathfinder2.geometry;
 
 public class Translation {
@@ -13,6 +23,17 @@ public class Translation {
         this.vz = vz;
     }
 
+    public static Translation absoluteToRelative(Translation translation,
+                                                 Angle heading) {
+        Angle newAngle = translation.angle().add(heading);
+
+        return new Translation(
+                newAngle.cos() * translation.magnitude(),
+                newAngle.sin() * translation.magnitude(),
+                translation.vz()
+        );
+    }
+
     public double vx() {
         return this.vx;
     }
@@ -23,5 +44,21 @@ public class Translation {
 
     public double vz() {
         return this.vz;
+    }
+
+    public double magnitude() {
+        return Math.hypot(this.vx, this.vy);
+    }
+
+    public PointXY point() {
+        return new PointXY(this.vx, this.vy);
+    }
+
+    public Angle angle() {
+        return PointXY.zero().angleTo(point());
+    }
+
+    public Translation toRelative(Angle heading) {
+        return absoluteToRelative(this, heading);
     }
 }
--- src/main/java/me/wobblyyyy/pathfinder2/units/Unit.java
+++ src/main/java/me/wobblyyyy/pathfinder2/units/Unit.java
@@ -1,3 +1,13 @@
+/*
+ * Copyright (c) 2021.
+ *
+ * This file is part of the "Pathfinder2" project, available here:
+ * <a href="https://github.com/Wobblyyyy/Pathfinder2">GitHub</a>
+ *
+ * This project is licensed under the GNU GPL V3 license.
+ * <a href="https://www.gnu.org/licenses/gpl-3.0.en.html">GNU GPL V3</a>
+ */
+
 package me.wobblyyyy.pathfinder2.units;
 
 public enum Unit {
