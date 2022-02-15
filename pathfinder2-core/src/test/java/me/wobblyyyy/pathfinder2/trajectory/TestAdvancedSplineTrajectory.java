4221a534e9a4a4ecf2d8d3baee511a07cceb6fab
Wobblyyyy <wobblyyyy@gmail.com>
2022-02-15 12:31:37 -0500

    add test for advanced spline trajectory

--- pathfinder2-core/src/test/java/me/wobblyyyy/pathfinder2/trajectory/TestAdvancedSplineTrajectory.java
+++ pathfinder2-core/src/test/java/me/wobblyyyy/pathfinder2/trajectory/TestAdvancedSplineTrajectory.java
@@ -3,6 +3,7 @@ package me.wobblyyyy.pathfinder2.trajectory;
 import me.wobblyyyy.pathfinder2.Pathfinder;
 import me.wobblyyyy.pathfinder2.geometry.Angle;
 import me.wobblyyyy.pathfinder2.geometry.PointXYZ;
+import me.wobblyyyy.pathfinder2.geometry.Translation;
 import me.wobblyyyy.pathfinder2.robot.simulated.SimulatedOdometry;
 import me.wobblyyyy.pathfinder2.trajectory.spline.AdvancedSplineTrajectoryBuilder;
 import org.junit.jupiter.api.Assertions;
@@ -20,13 +21,32 @@ public class TestAdvancedSplineTrajectory {
                 .setAngleTolerance(Angle.fromDeg(5))
                 .add(new PointXYZ(0, 0, 0))
                 .add(new PointXYZ(10, 10, 0))
+                .add(new PointXYZ(11, 12, 0))
                 .build();
 
         pathfinder.followTrajectory(trajectory);
         pathfinder.tick();
-
+        Assertions.assertTrue(pathfinder.isActive());
+        Assertions.assertEquals(
+                new Translation(0.354, 0.356, -0.018),
+                pathfinder.getTranslation()
+        );
+        PointXYZ current = pathfinder.getPosition();
+        Assertions.assertEquals(0.5, trajectory.speed(current));
         odometry.setRawPosition(new PointXYZ(10, 10, 0));
         pathfinder.tick();
+        Assertions.assertTrue(pathfinder.isActive());
+        Assertions.assertNotEquals(
+                new Translation(0.354, 0.356, -0.018),
+                pathfinder.getTranslation()
+        );
+        Assertions.assertEquals(0.5, trajectory.speed(current));
+        odometry.setRawPosition(new PointXYZ(11, 12, 0));
+        pathfinder.tick();
+        Assertions.assertEquals(
+                new Translation(0, 0, 0),
+                pathfinder.getTranslation()
+        );
         Assertions.assertFalse(pathfinder.isActive());
     }
 }
