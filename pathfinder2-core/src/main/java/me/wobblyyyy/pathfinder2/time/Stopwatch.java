485980ab48a6e7eeef231433d6ca0c5c3e8863ad
Wobblyyyy <wobblyyyy@gmail.com>
2022-02-15 19:38:38 -0500

    fix a typo lol

--- pathfinder2-core/src/main/java/me/wobblyyyy/pathfinder2/time/Stopwatch.java
+++ pathfinder2-core/src/main/java/me/wobblyyyy/pathfinder2/time/Stopwatch.java
@@ -14,7 +14,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * A stopwatch, capable of stopping AND watching. It's absolutely bind
+ * A stopwatch, capable of stopping AND watching. It's absolutely mind
  * bending how incredibly complex such a thing is.
  *
  * <p>
