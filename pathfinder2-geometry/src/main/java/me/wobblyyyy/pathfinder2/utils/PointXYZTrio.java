6a1d88cbbce150b7782262ba936593414c8194c6
Wobblyyyy <wobblyyyy@gmail.com>
2022-02-19 14:07:31 -0500

    another competition day down

---
+++ pathfinder2-geometry/src/main/java/me/wobblyyyy/pathfinder2/utils/PointXYZTrio.java
@@ -0,0 +1,21 @@
+/*
+ * Copyright (c) 2021.
+ *
+ * This file is part of the "Pathfinder2" project, available here:
+ * <a href="https://github.com/Wobblyyyy/Pathfinder2">GitHub</a>
+ *
+ * This project is licensed under the GNU GPL V3 license.
+ * <a href="https://www.gnu.org/licenses/gpl-3.0.en.html">GNU GPL V3</a>
+ */
+
+package me.wobblyyyy.pathfinder2.utils;
+
+import me.wobblyyyy.pathfinder2.geometry.PointXYZ;
+
+public class PointXYZTrio extends Trio<PointXYZ, PointXYZ, PointXYZ> {
+    public PointXYZTrio(PointXYZ a,
+                        PointXYZ b,
+                        PointXYZ c) {
+        super(a, b, c);
+    }
+}
