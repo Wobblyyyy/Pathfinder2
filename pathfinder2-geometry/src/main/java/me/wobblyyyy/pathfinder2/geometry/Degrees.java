c307f14a3471e1ef1617197ea2e77e836ce441d8
Wobblyyyy <wobblyyyy@gmail.com>
2022-02-26 11:14:27 -0500

    add Degrees and Radians

---
+++ pathfinder2-geometry/src/main/java/me/wobblyyyy/pathfinder2/geometry/Degrees.java
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) 2021.
+ *
+ * This file is part of the "Pathfinder2" project, available here:
+ * <a href="https://github.com/Wobblyyyy/Pathfinder2">GitHub</a>
+ *
+ * This project is licensed under the GNU GPL V3 license.
+ * <a href="https://www.gnu.org/licenses/gpl-3.0.en.html">GNU GPL V3</a>
+ */
+
+package me.wobblyyyy.pathfinder2.geometry;
+
+/**
+ * Angle measurement in degrees.
+ * 
+ * @since 1.3.0
+ */
+public class Degrees extends Angle {
+    public Degrees(double degrees) {
+        super(degrees);
+    }
+}
