d01bfb12932b6993808c0aa78d1a5f31457eff91
Wobblyyyy <wobblyyyy@gmail.com>
2022-03-16 16:19:49 -0400

    add package-info.java for pathfinder2-commands

---
+++ pathfinder2-commands/src/main/java/me/wobblyyyy/pathfinder2/commands/package-info.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2021.
+ *
+ * This file is part of the "Pathfinder2" project, available here:
+ * <a href="https://github.com/Wobblyyyy/Pathfinder2">GitHub</a>
+ *
+ * This project is licensed under the GNU GPL V3 license.
+ * <a href="https://www.gnu.org/licenses/gpl-3.0.en.html">GNU GPL V3</a>
+ */
+
+/**
+ * The {@code pathfinder2-commands} module provides rudimentary scripting
+ * capabilities for Pathfinder, allowing you to write out autonomous programs
+ * in pseudo-plain-text form.
+ */
+package me.wobblyyyy.pathfinder2.commands;
