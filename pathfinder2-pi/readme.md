c410ea1005f70029ae191a2e31434104d3752cca
Wobblyyyy <wobblyyyy@gmail.com>
2022-03-22 14:04:19 -0400

    start adding pi support

---
+++ pathfinder2-pi/readme.md
@@ -0,0 +1,19 @@
+<p align="center">
+<img src="../media/pathfinder2-logo.png" alt="Pathfinder2">
+<br>
+<i>Autonomous motion planning and control library for wheeled mobile robots.</i>
+<br>
+<i>Successor to <a href="https://github.com/Wobblyyyy/Pathfinder">Pathfinder</a>.</i>
+</p>
+
+# pathfinder2-pi
+Pathfinder-related code designed for use with the Raspberry Pi. This is an
+experimental module and will likely be unstable for the foreseeable future.
+
+## Dependencies
+
+This module depends on...
+
+- `pathfinder2-kinematics`
+- `pathfinder2-geometry`
+- `pathfinder2-core`
---
+++ pathfinder2-pi/src/main/java/me/wobblyyyy/pathfinder2/pi/InputPin.java
@@ -0,0 +1,36 @@
+package me.wobblyyyy.pathfinder2.pi;
+
+import com.pi4j.context.Context;
+import com.pi4j.io.gpio.digital.DigitalInput;
+import com.pi4j.io.gpio.digital.DigitalInputConfig;
+import com.pi4j.io.gpio.digital.PullResistance;
+
+public class InputPin {
+    private final DigitalInputConfig config;
+    private final DigitalInput input;
+
+    public InputPin(
+        Context context,
+        String id,
+        String name,
+        int address,
+        PullResistance pullResistance,
+        long debounce,
+        String provider
+    ) {
+        this.config = DigitalInput.newConfigBuilder(context)
+            .id(id)
+            .name(name)
+            .address(address)
+            .pull(pullResistance)
+            .debounce(debounce)
+            .provider(provider)
+            .build();
+
+        this.input = context.create(this.config);
+    }
+
+    public DigitalInput getInput() {
+        return input;
+    }
+}
