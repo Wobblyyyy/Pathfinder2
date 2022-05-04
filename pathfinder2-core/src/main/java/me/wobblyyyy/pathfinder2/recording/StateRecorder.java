480cc6cfc8e6c5584988191f216cde6e33d22c04
Wobblyyyy <wobblyyyy@gmail.com>
2022-05-04 17:30:57 -0400

    documentation

--- pathfinder2-core/src/main/java/me/wobblyyyy/pathfinder2/recording/StateRecorder.java
+++ pathfinder2-core/src/main/java/me/wobblyyyy/pathfinder2/recording/StateRecorder.java
@@ -23,7 +23,36 @@ import me.wobblyyyy.pathfinder2.utils.ValidationUtils;
 /**
  * Used in recording the state of a robot. If you've already recorded a
  * state, you can then play that state back. Additionally, you can play
- * back a timed set of states.
+ * back a timed set of states. This functions by using a map containing
+ * {@link Recordable}s. Each {@link Recordable}, as its name would suggest,
+ * can be recorded. To add, each {@link Recordable} can play back an existing
+ * recording.
+ *
+ * <p>
+ * A {@code StateRecorder} exists in one of three states:
+ * <ul>
+ *     <li>Idle</li>
+ *     <li>Recording</li>
+ *     <li>Playing back</li>
+ * </ul>
+ * By default, it's in the Idle state. Recording and playback can be
+ * started and stoppeed using the following methods:
+ * <ul>
+ *     <li>{@link #startRecording(int, int)}</li>
+ *     <li>{@link #stopRecording()}</li>
+ *     <li>{@link #startPlayback(StateRecording)}</li>
+ *     <li>{@link #stopPlayback()}</li>
+ * </ul>
+ * </p>
+ *
+ * <p>
+ * By default, {@code StateRecorder} will automatically "optimize" ongoing
+ * recordings by removing redundant information. Specifically, if two adjacent
+ * records have the same value for a {@code Recordable}, only the first of
+ * those records will be preserved. This helps to cut down on recording output
+ * size. This functionality can be configured by changing a flag with
+ * {@link #setShouldOptimize(boolean)}.
+ * </p>
  *
  * @author Colin Robertson
  * @since 2.4.0
@@ -68,10 +97,22 @@ public class StateRecorder {
         return this;
     }
 
+    /**
+     * Remove a node based on a {@code String} key.
+     *
+     * @param key the key of the node to remove.
+     * @return the removed node. If the node was not contained in the map,
+     * this method will return null.
+     */
     public Recordable<?> removeNode(String key) {
         return nodes.remove(key);
     }
 
+    /**
+     * Get all of the recordable nodes in the {@code StateRecorder}.
+     *
+     * @return {@link #nodes}.
+     */
     public Map<String, Recordable<?>> getNodes() {
         return nodes;
     }
@@ -163,6 +204,36 @@ public class StateRecorder {
         return this;
     }
 
+    /**
+     * Start a recording. Note that this will overwrite any existing recording.
+     * A recording cannot be started if there is already an ongoing recording.
+     * Additionally, a recording cannot be started if a recording is currently
+     * being played back. Use {@link #stopRecording()} to stop recording.
+     *
+     * <p>
+     * After finishing a recording, use {@link #stopRecording()} to stop
+     * the recording from being continued. This method will return a
+     * {@link StateRecording}, which you can save elsewhere. If you'd like to
+     * access this recording later, use {@link #getRecording()}. Please note
+     * that if you start another recording, or if you start the playback of
+     * another recording, {@link #getRecording()} will return that recording
+     * instead.
+     * </p>
+     *
+     * @param recordingIntervalMs        the interval, in milliseconds,
+     *                                   between each record. A higher
+     *                                   interval decreases the accuracy of
+     *                                   the recording, while a lower interval
+     *                                   increases the size of the outputted
+     *                                   recording. A good place to start
+     *                                   is 100.
+     * @param estimatedRecordingLengthMs how long the recording is estimated
+     *                                   to last, in milliseconds. This value
+     *                                   doesn't matter all that much - it's
+     *                                   just used in allocating an array for
+     *                                   the list of records.
+     * @return {@code this}, used for method chaining.
+     */
     public StateRecorder startRecording(
         int recordingIntervalMs,
         int estimatedRecordingLengthMs
@@ -188,12 +259,25 @@ public class StateRecorder {
         return this;
     }
 
+    /**
+     * Set the {@link #isRecording} flag to false, stopping any ongoing
+     * recording.
+     *
+     * @return the recording that was just stopped.
+     */
     public StateRecording stopRecording() {
         isRecording = false;
 
         return recording;
     }
 
+    /**
+     * Start the playback of a given recording. This will overwrite any
+     * existing recording.
+     *
+     * @param recording the recording to play.
+     * @return {@code this}, used for method chaining.
+     */
     public StateRecorder startPlayback(StateRecording recording) {
         ValidationUtils.validate(recording, "recording");
 
@@ -216,13 +300,46 @@ public class StateRecorder {
         return this;
     }
 
+    /**
+     * Stop any ongoing playback by setting the {@link #isPlayingBack} flag
+     * to {@code false}.
+     *
+     * @return {@code this}, used for method chaining.
+     */
     public StateRecorder stopPlayback() {
         isPlayingBack = false;
 
         return this;
     }
 
-    private void cleanseRecord(StateRecord record) {}
+    private void cleanseRecord(StateRecord record, StateRecord lastRecord) {
+        if (shouldOptimize) {
+            if (lastRecord != null) {
+                Map<String, Object> lastMap = lastRecord.getMap();
+                Map<String, Object> map = record.getMap();
+
+                List<String> toRemove = new ArrayList<>(map.size());
+                for (Map.Entry<String, Object> entry : map.entrySet()) {
+                    String key = entry.getKey();
+                    Object value = entry.getValue();
+
+                    if (!lastMap.containsKey(key)) {
+                        continue;
+                    }
+
+                    Object lastValue = lastMap.get(key);
+
+                    if (lastValue.equals(value)) {
+                        toRemove.add(key);
+                    }
+                }
+
+                for (String str : toRemove) {
+                    map.remove(str);
+                }
+            }
+        }
+    }
 
     /**
      * Update the {@code StateRecorder}. If a recording is currently underway,
@@ -245,32 +362,7 @@ public class StateRecorder {
                 lastRecord = recording.getLastRecord();
                 recording.addRecord(record);
 
-                if (shouldOptimize) {
-                    if (lastRecord != null) {
-                        Map<String, Object> lastMap = lastRecord.getMap();
-                        Map<String, Object> map = record.getMap();
-
-                        List<String> toRemove = new ArrayList<>(map.size());
-                        for (Map.Entry<String, Object> entry : map.entrySet()) {
-                            String key = entry.getKey();
-                            Object value = entry.getValue();
-
-                            if (!lastMap.containsKey(key)) {
-                                continue;
-                            }
-
-                            Object lastValue = lastMap.get(key);
-
-                            if (lastValue.equals(value)) {
-                                toRemove.add(key);
-                            }
-                        }
-
-                        for (String str : toRemove) {
-                            map.remove(str);
-                        }
-                    }
-                }
+                cleanseRecord(record, lastRecord);
             } else if (isPlayingBack) {
                 int recordingSize = recording.getRecords().size();
 
@@ -289,12 +381,28 @@ public class StateRecorder {
         }
     }
 
+    /**
+     * Update the {@code StateRecorder}. If a recording is currently underway,
+     * this will record the robot's current state and add it to the ongoing
+     * recording. If the {@code StateRecorder} is currently playing back
+     * an existing recording, this will play back the next "frame" of the
+     * recording.
+     */
     public void update() {
         double currentTimeMs = Time.ms();
 
         update(currentTimeMs);
     }
 
+    /**
+     * Get the {@code StateRecorder}'s current recording. If a recording
+     * is currently underway, this method will return that recording. If a
+     * recording was recorded previously and {@code startPlayback} has not
+     * been called, this method will return that recording. If playback
+     * has started, this will return the recording being played back.
+     *
+     * @return the {@code StateRecorder}'s current recording.
+     */
     public StateRecording getRecording() {
         return recording;
     }
@@ -319,6 +427,22 @@ public class StateRecorder {
         return isPlayingBack;
     }
 
+    /**
+     * Set if the {@code StateRecorder} should automatically optimize the
+     * recording by removing redundant records. This helps to save recording
+     * size by reducing the amount of records in the recording.
+     *
+     * <p>
+     * If optimization is enabled, and a {@code Motor} was recorded having
+     * the same power value for two adjacent records, the second record would
+     * be removed. Because it has the same value as the first record, there's
+     * no point in having another record.
+     * </p>
+     *
+     * @param shouldOptimize true if optimization should be enabled. Otherwise,
+     *                       false. Optimization is enabled by default.
+     * @return {@code this}, used for method chaining.
+     */
     public StateRecorder setShouldOptimize(boolean shouldOptimize) {
         this.shouldOptimize = shouldOptimize;
 
